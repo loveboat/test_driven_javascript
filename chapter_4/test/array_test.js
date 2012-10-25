bdfd95e90bd36aa42e5c00e31196443a65e2f68a
David Lusby <lusbyd@gmail.com>
2012-10-26 00:17:09 +0100

    Fixed test to match result and new knowledge

--- chapter_4/test/array_test.js
+++ chapter_4/test/array_test.js
@@ -4,9 +4,9 @@ TestCase("ArrayTest", {
     var result = arr.splice(2, 3);
     assertEquals([1, 2], arr);
   },
-	"test array splice should return modified array": function () {
+	"test array splice should return removed items": function () {
 	  var arr = [1, 2, 3, 4, 5];
 	  var result = arr.splice(2, 3);
-	  assertEquals(arr, result);
+	  assertEquals([3, 4, 5], result);
 	}
 });
